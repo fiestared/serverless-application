9044fbe6859f3900ea50779f2df2375cf157f11a
fiestared <back.in.fiestared@gmail.com>
2019-07-31 09:46:15 +0900

    in progess, adding date and tofilter remained

--- src/handlers/me/wallet/token/allhistories/index/handler.py
+++ src/handlers/me/wallet/token/allhistories/index/handler.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 import boto3
-from me_wallet_token_histories_index import MeWalletAllTokenHistoriesIndex
+from me_wallet_all_token_histories_index import MeWalletAllTokenHistoriesIndex
 
 cognito = boto3.client('cognito-idp')
 dynamodb = boto3.resource('dynamodb')
 
 
 def lambda_handler(event, context):
-    me_wallet_token_histories_index = MeWalletAllTokenHistoriesIndex(event, context, dynamodb, cognito=cognito)
-    return me_wallet_token_histories_index.main()
+    me_wallet_all_token_histories_index = MeWalletAllTokenHistoriesIndex(event, context, dynamodb, cognito=cognito)
+    return me_wallet_all_token_histories_index.main()
--- src/handlers/me/wallet/token/allhistories/index/me_wallet_all_token_histories_index.py
+++ src/handlers/me/wallet/token/allhistories/index/me_wallet_all_token_histories_index.py
@@ -7,16 +7,6 @@ from user_util import UserUtil
 from web3 import Web3, HTTPProvider
 from lambda_base import LambdaBase
 
-# from token_distributer_base.py
-#from abc import *
-#import os
-#import boto3
-#from web3 import Web3, HTTPProvider
-#from lambda_base import LambdaBase
-#from boto3.dynamodb.conditions import Key
-#import copy
-#from db_util import DBUtil
-
 class MeWalletAllTokenHistoriesIndex(LambdaBase):
 
     def get_schema(self):
@@ -30,61 +20,91 @@ class MeWalletAllTokenHistoriesIndex(LambdaBase):
     def exec_main_proc(self):
         self.web3 = Web3(HTTPProvider(os.environ['PRIVATE_CHAIN_OPERATION_URL']))
         address = self.web3.toChecksumAddress(os.environ['PRIVATE_CHAIN_ALIS_TOKEN_ADDRESS'])
-
-        # lambda on VPCの要素が見当たらないが、、設定ファイルを見る必要があるか？
-        # eoaの変数代入が必要、ユーザのprivate_eth_address
-        eoa = self.__get_user_private_eth_address(self.params['user_id'])
-
-        fromBlock = 1
+        user_id = self.event['requestContext']['authorizer']['claims']['cognito:username']
+        eoa = self.__get_user_private_eth_address(user_id)
         
         def padLeft(eoa):
-            return '0x000000000000000000000000' + eoa[1:26]
+            return '0x000000000000000000000000' + eoa[2:]
 
         def removeLeft(eoa):
-            return '0x' + eoa[1:26]
+            return '0x' + eoa[26:]
 
-        def filterdata(transfer_result):
-            for i in result.length:
-                #このままではunixtimeが入ってしまうので、後ほどyyyy/mm/dd ttに変更する
-                date = web3.eth.getBlock(web3.eth.getBlock(result[i].blockNumber).timestamp*1000)
-                
-                #print関数にしているが、S3にextractできるように将来調整が必要
-                print('%s,%s,%s,%s,%s', date,result[i].transactionHash,removeLeft(result[i].topics[1]),
-                    removeLeft(result[i].topics[2]), web3.fromWei(result[i].data, 'ether'))
+        def filter_transfer_data(transfer_result):
+            for i in range(len(transfer_result)):
+                ### Dateを追加する
+                print("%s,%s,%s,%s"%(
+                    transfer_result[i]['transactionHash'].hex(),
+                    removeLeft(transfer_result[i]['topics'][1].hex()),
+                    removeLeft(transfer_result[i]['topics'][2].hex()),
+                    self.web3.fromWei(int(transfer_result[i]['data'], 16), 'ether')
+                    )   
+                    )
 
-        def getTransferHistory(address, fromBlock, eoa):
-            fromfilter = web3.eth.filter({ address: address,
-                            fromBlock: fromBlock,
-                            toBlock: 'latest',
-                            topics: [
-                                web3.sha3('Transfer(address,address,uint256)'), padLeft(eoa)
-                            ]
-                        })
+        def filter_mint_data(mint_result):
+            for i in range(len(mint_result)):
+                ### Dateを追加する
+                print("%s,%s,%s,%s"%(
+                    mint_result[i]['transactionHash'].hex(),
+                    '---',
+                    removeLeft(mint_result[i]['topics'][1].hex()),
+                    self.web3.fromWei(int(mint_result[i]['data'], 16), 'ether')
+                    )   
+                    )
 
-            tofilter = web3.eth.filter({ address: address,
-                            fromBlock: fromBlock,
-                            toBlock: 'latest',
-                            topics: [
-                                web3.sha3('Transfer(address,address,uint256)'),, padLeft(eoa)
-                            ]
-                        })
+        def getTransferHistory(address, eoa):
+            fromfilter = self.web3.eth.filter({
+                "address": address,
+                "fromBlock": 1,
+                "toBlock": 'latest',
+                "topics": [self.web3.sha3(text="Transfer(address,address,uint256)").hex(),
+                           [padLeft(eoa)]
+                ],
+                })
 
-            # ステータスコードを確認して、失敗していたら例外を投げる処理を後々追加
-            transfer_result_from = fromfilter.get
-            filterdata(transfer_result_from)
+            ### ステータスコードを確認して、失敗していたら例外を投げる処理を後々追加する
+            transfer_result_from = fromfilter.get_all_entries()
+            filter_transfer_data(transfer_result_from)
 
-            # ステータスコードを確認して、失敗していたら例外を投げる処理を後々追加
-            transfer_result_to = tofilter.get
-            filterdata(transfer_result_to)
+            tofilter = self.web3.eth.filter({
+                "address": address,
+                "fromBlock": 1,
+                "toBlock": 'latest',
+                "topics": [self.web3.sha3(text="Transfer(address,address,uint256)").hex(),
+                            # ここの１つ目の要素の任意指定のみ。公式ドキュメントではNone,nullとされているが、pythonだとsyntaxerrorになってしまう。
+                            # jsではweb3.sha3('Transfer(address,address,uint256)'),, padLeft(eoa)と、空表記にしている
+                            # 究極は、全部のデータを持ってきてtopicsのtoの部分がeoaと一致する数字が特定のものだけをtransfer_result_toに追加するとか？
+#                            ['', padLeft(eoa)]
+                ],
+            })
 
-        def __get_user_private_eth_address(self, user_id):
-            # user_id に紐づく private_eth_address を取得
-            user_info = UserUtil.get_cognito_user_info(self.cognito, user_id)
-            private_eth_address = [a for a in user_info['UserAttributes'] if a.get('Name') == 'custom:private_eth_address']
-            # private_eth_address が存在しないケースは想定していないため、取得出来ない場合は例外とする
-            if len(private_eth_address) != 1:
-                raise RecordNotFoundError('Record Not Found: private_eth_address')
+            ### ステータスコードを確認して、失敗していたら例外を投げる処理を後々追加する
+            transfer_result_to = tofilter.get_all_entries()
+            filter_transfer_data(transfer_result_to)
 
-            return private_eth_address[0]['Value']
+        def getMintHistory(address, eoa):
+            
+            to_filter = self.web3.eth.filter({
+                "address": address,
+                "fromBlock": 1,
+                "toBlock": 'latest',
+                "topics": [self.web3.sha3(text="Mint(address,uint256)").hex(),
+                            [padLeft(eoa)]
+                ],
+            })
 
-        getTransferHistory(address, fromBlock, eoa)
+            ### ステータスコードを確認して、失敗していたら例外を投げる処理を後々追加する
+            mint_result = to_filter.get_all_entries()
+            filter_mint_data(mint_result)
+
+        getTransferHistory(address, eoa)
+        getMintHistory(address, eoa)
+
+    def __get_user_private_eth_address(self, user_id):
+        # user_id に紐づく private_eth_address を取得
+        user_info = UserUtil.get_cognito_user_info(self.cognito, user_id)
+        private_eth_address = [a for a in user_info['UserAttributes'] if a.get('Name') == 'custom:private_eth_address']
+        # private_eth_address が存在しないケースは想定していないため、取得出来ない場合は例外とする
+        if len(private_eth_address) != 1:
+            raise RecordNotFoundError('Record Not Found: private_eth_address')
+
+        return private_eth_address[0]['Value']
