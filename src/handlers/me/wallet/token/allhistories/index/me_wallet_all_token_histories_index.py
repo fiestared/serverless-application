3822209fb76eec1fff9104753ed64744e7ba3fe6
fiestared <back.in.fiestared@gmail.com>
2019-08-15 04:11:15 +0900

    added types and refactored some codes

--- src/handlers/me/wallet/token/allhistories/index/me_wallet_all_token_histories_index.py
+++ src/handlers/me/wallet/token/allhistories/index/me_wallet_all_token_histories_index.py
@@ -6,14 +6,15 @@ import time
 import csv
 import boto3
 import hashlib
-from datetime import datetime
+from datetime import datetime, timedelta, timezone
 from time_util import TimeUtil
 from user_util import UserUtil
 from web3 import Web3, HTTPProvider
 from lambda_base import LambdaBase
 
 ### TODO:全体的なCodingは他に合わせて修正
-### TODO:通知のアイコンをダウンロードマークに修正する
+### TODO:例外処理を追加する→富樫さんに確認
+### TODO:ユーザーのidentitypoolidをファイル名に挿入する
 class MeWalletAllTokenHistoriesIndex(LambdaBase):
 
     def get_schema(self):
@@ -23,137 +24,6 @@ class MeWalletAllTokenHistoriesIndex(LambdaBase):
         UserUtil.verified_phone_and_email(self.event)
 
     web3 = None
-
-    def exec_main_proc(self):
-        self.web3 = Web3(HTTPProvider(os.environ['PRIVATE_CHAIN_OPERATION_URL']))
-        address = self.web3.toChecksumAddress(os.environ['PRIVATE_CHAIN_ALIS_TOKEN_ADDRESS'])
-        user_id = self.event['requestContext']['authorizer']['claims']['cognito:username']
-        eoa = self.__get_user_private_eth_address(user_id)
-
-        tmp_csv_file = '/tmp/tmp_csv_file.csv'
-        f = open(tmp_csv_file, 'a')
-        writer = csv.writer(f)
-        
-        def padLeft(eoa):
-            return '0x000000000000000000000000' + eoa[2:]
-
-        def removeLeft(eoa):
-            return '0x' + eoa[26:]
-
-        ### TODO:特定のユーザのみがアクセス可能なS3のbacketを作成する
-        def filter_transfer_data(transfer_result):
-            for i in range(len(transfer_result)):
-                writer.writerow([
-                    datetime.fromtimestamp(self.web3.eth.getBlock(transfer_result[i]['blockNumber'])['timestamp']),
-                    transfer_result[i]['transactionHash'].hex(),
-                    removeLeft(transfer_result[i]['topics'][1].hex()),
-                    removeLeft(transfer_result[i]['topics'][2].hex()),
-                    self.web3.fromWei(int(transfer_result[i]['data'], 16), 'ether')
-                ])
-
-            ### debug用
-#                print("%s,%s,%s,%s,%s"%(
-#                    datetime.fromtimestamp(self.web3.eth.getBlock(transfer_result[i]['blockNumber'])['timestamp']),
-#                    transfer_result[i]['transactionHash'].hex(),
-#                    removeLeft(transfer_result[i]['topics'][1].hex()),
-#                    removeLeft(transfer_result[i]['topics'][2].hex()),
-#                    self.web3.fromWei(int(transfer_result[i]['data'], 16), 'ether')
-#                    )   
-#                    )
-
-        def filter_mint_data(mint_result):
-            for i in range(len(mint_result)):
-                writer.writerow([
-                    datetime.fromtimestamp(self.web3.eth.getBlock(mint_result[i]['blockNumber'])['timestamp']),
-                    mint_result[i]['transactionHash'].hex(),
-                    '---',
-                    removeLeft(mint_result[i]['topics'][1].hex()),
-                    self.web3.fromWei(int(mint_result[i]['data'], 16), 'ether')
-                ])
-            
-            ### debug用
-#            f.close()
-#            f2 = open('/tmp/sample.csv', 'r')
-#            readf = csv.reader(f2)
-#            for row in readf:
-#                print(row)
-
-            ### debug用
-#                print("%s,%s,%s,%s,%s"%(
-#                    datetime.fromtimestamp(self.web3.eth.getBlock(transfer_result[i]['blockNumber'])['timestamp']),
-#                    mint_result[i]['transactionHash'].hex(),
-#                    '---',
-#                    removeLeft(mint_result[i]['topics'][1].hex()),
-#                    self.web3.fromWei(int(mint_result[i]['data'], 16), 'ether')
-#                    )   
-#                    )
-
-        def getTransferHistory(address, eoa):
-            fromfilter = self.web3.eth.filter({
-                "address": address,
-                "fromBlock": 1,
-                "toBlock": 'latest',
-                "topics": [self.web3.sha3(text="Transfer(address,address,uint256)").hex(),
-                           padLeft(eoa)
-                ],
-                })
-
-            tofilter = self.web3.eth.filter({
-                "address": address,
-                "fromBlock": 1,
-                "toBlock": 'latest',
-                "topics": [self.web3.sha3(text="Transfer(address,address,uint256)").hex(),
-                            None,
-                            padLeft(eoa)
-                ],
-            })
-            ### ステータスコードを確認して、失敗していたら例外を投げる処理を後々追加する
-            transfer_result_from = fromfilter.get_all_entries()
-            filter_transfer_data(transfer_result_from)
-
-            transfer_result_to = tofilter.get_all_entries()
-            filter_transfer_data(transfer_result_to)
-
-        def getMintHistory(address, eoa):            
-            to_filter = self.web3.eth.filter({
-                "address": address,
-                "fromBlock": 1,
-                "toBlock": 'latest',
-                "topics": [self.web3.sha3(text="Mint(address,uint256)").hex(),
-                            padLeft(eoa)
-                ],
-            })
-
-            ### ステータスコードを確認して、失敗していたら例外を投げる処理を後々追加する
-            mint_result = to_filter.get_all_entries()
-            filter_mint_data(mint_result)
-
-        def extract_file_to_s3():
-            bucket = 'alis-yasu-staging'
-            key = user_id + '_' + datetime.now().strftime('%Y-%m-%d-%H-%M-%S') + '.csv'
-            with open(tmp_csv_file, 'rb') as f:
-                csv_file = f.read()
-                res = upload_file(bucket, key, csv_file)
-            
-            announce_url = 'https://'+bucket+'.s3-ap-northeast-1.amazonaws.com/'+key
-            return announce_url
-
-        def upload_file(bucket, key, bytes):
-            s3 = boto3.resource('s3')
-            s3Obj = s3.Object(bucket, key)
-            res = s3Obj.put(Body = bytes)
-            return res
-
-        getTransferHistory(address, eoa)
-        getMintHistory(address, eoa)
-        f.close()
-        announce_url = extract_file_to_s3()
-
-        self.__notification(user_id, announce_url)
-
-        return {
-            'statusCode': 200
-        }
 
     def __get_user_private_eth_address(self, user_id):
         # user_id に紐づく private_eth_address を取得
@@ -181,11 +51,145 @@ class MeWalletAllTokenHistoriesIndex(LambdaBase):
             'notification_id': self.__get_randomhash(),
             'user_id': user_id,
             'sort_key': TimeUtil.generate_sort_key(),
-            'type': settings.ANNOUNCE_NOTIFICATION_TYPE,
+            'type': settings.CSVDOWNLOAD_NOTIFICATION_TYPE,
             'created_at': int(time.time()),
-            'announce_body': '全トークン履歴のcsvのダウンロード準備が完了しました',
+            'announce_body': '全トークン履歴のcsvのダウンロード準備が完了しました。本通知をクリックしてダウンロードしてください。',
             'announce_url': announce_url
         })
 
+        self.__update_unread_notification_manager(user_id)
+
     def __get_randomhash(self):
         return hashlib.sha256((str(time.time()) + str(os.urandom(16))).encode('utf-8')).hexdigest()
+
+    def exec_main_proc(self):
+        self.web3 = Web3(HTTPProvider(os.environ['PRIVATE_CHAIN_OPERATION_URL']))
+        address = self.web3.toChecksumAddress(os.environ['PRIVATE_CHAIN_ALIS_TOKEN_ADDRESS'])
+        user_id = self.event['requestContext']['authorizer']['claims']['cognito:username']
+        eoa = self.__get_user_private_eth_address(user_id)
+
+        tmp_csv_file = '/tmp/tmp_csv_file.csv'
+        f = open(tmp_csv_file, 'a')
+        writer = csv.writer(f)
+        
+        def padLeft(eoa):
+            return '0x000000000000000000000000' + eoa[2:]
+
+        def removeLeft(eoa):
+            return '0x' + eoa[26:]
+
+        def add_type(from_eoa, to_eoa):
+            if from_eoa == eoa and to_eoa == '0x20326c2C26C5F5D314316131d815eb92940e761A':
+                return 'withdraw'
+            elif from_eoa == eoa and to_eoa != '0x0000000000000000000000000000000000000000':
+                return 'give'
+            elif from_eoa == eoa and to_eoa == '0x0000000000000000000000000000000000000000':
+                return 'burn'
+            elif from_eoa == '0x20326c2C26C5F5D314316131d815eb92940e761A' and to_eoa == eoa:
+                return 'deposit'
+            elif from_eoa == '---' and to_eoa == eoa:
+                return 'get by like'            
+            elif from_eoa != '0x20326c2C26C5F5D314316131d815eb92940e761A' and to_eoa == eoa:
+                return 'get from user'
+            else:
+                return 'unknown'
+
+        def filter_transfer_data(transfer_result):
+            for i in range(len(transfer_result)):
+                writer.writerow([
+                    datetime.fromtimestamp(self.web3.eth.getBlock(transfer_result[i]['blockNumber'])['timestamp']),
+                    transfer_result[i]['transactionHash'].hex(),
+                    add_type(removeLeft(transfer_result[i]['topics'][1].hex()),removeLeft(transfer_result[i]['topics'][2].hex())),
+                    self.web3.fromWei(int(transfer_result[i]['data'], 16), 'ether')
+                ])
+
+        def filter_mint_data(mint_result):
+            for i in range(len(mint_result)):
+                writer.writerow([
+                    datetime.fromtimestamp(self.web3.eth.getBlock(mint_result[i]['blockNumber'])['timestamp']),
+                    mint_result[i]['transactionHash'].hex(),
+                    add_type('---', removeLeft(mint_result[i]['topics'][1].hex())),
+                    self.web3.fromWei(int(mint_result[i]['data'], 16), 'ether')
+                ])
+            
+        def getTransferHistory(address, eoa):
+            fromfilter = self.web3.eth.filter({
+                "address": address,
+                "fromBlock": 1,
+                "toBlock": 'latest',
+                "topics": [self.web3.sha3(text="Transfer(address,address,uint256)").hex(),
+                           padLeft(eoa)
+                ],
+                })
+
+            tofilter = self.web3.eth.filter({
+                "address": address,
+                "fromBlock": 1,
+                "toBlock": 'latest',
+                "topics": [self.web3.sha3(text="Transfer(address,address,uint256)").hex(),
+                            None,
+                            padLeft(eoa)
+                ],
+            })
+            
+            ### ステータスコードを確認して、失敗していたら例外を投げる処理を後々追加する
+            try:
+                transfer_result_from = fromfilter.get_all_entries()
+                filter_transfer_data(transfer_result_from)
+                transfer_result_to = tofilter.get_all_entries()
+                filter_transfer_data(transfer_result_to)    
+            except ClientError as e:
+                    logging.fatal(e)
+                    return {
+                        'statusCode': 500,
+                        'body': json.dumps({'message': 'Internal server error'})
+                    }
+
+        def getMintHistory(address, eoa):            
+            to_filter = self.web3.eth.filter({
+                "address": address,
+                "fromBlock": 1,
+                "toBlock": 'latest',
+                "topics": [self.web3.sha3(text="Mint(address,uint256)").hex(),
+                            padLeft(eoa)
+                ],
+            })
+
+            try:
+                mint_result = to_filter.get_all_entries()
+                filter_mint_data(mint_result)
+            except ClientError as e:
+                    logging.fatal(e)
+                    return {
+                        'statusCode': 500,
+                        'body': json.dumps({'message': 'Internal server error'})
+                    }
+
+        def extract_file_to_s3():
+            bucket = os.environ['ALL_TOKEN_HISTORY_CSV_DWONLOAD_S3_BUCKET']
+            JST = timezone(timedelta(hours=+9), 'JST')
+            ### TODO:keyの${cognito-identity.amazonaws.com:sub}は別途取得が必要
+            key = 'private/ap-northeast-1:6eb2fb02-33b3-42de-8b9a-60d04afe3535/' + user_id + '_' + datetime.now(JST).strftime('%Y-%m-%d-%H-%M-%S') + '.csv'
+            with open(tmp_csv_file, 'rb') as f:
+                csv_file = f.read()
+                res = upload_file(bucket, key, csv_file)
+            
+            announce_url = 'https://'+bucket+'.s3-ap-northeast-1.amazonaws.com/'+key
+            return announce_url
+
+        def upload_file(bucket, key, bytes):
+            s3 = boto3.resource('s3')
+            s3Obj = s3.Object(bucket, key)
+            res = s3Obj.put(Body = bytes)
+            return res
+
+        getTransferHistory(address, eoa)
+        getMintHistory(address, eoa)
+        f.close()
+        announce_url = extract_file_to_s3()
+
+        self.__notification(user_id, announce_url)
+
+        return {
+            'statusCode': 200
+        }
